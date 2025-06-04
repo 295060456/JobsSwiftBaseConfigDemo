42e8b9d3efccaf7710e2e0d169ea66be39ccc814
Jobs <lg295060456@gmail.com>
2025-06-04 10:50:43 +0800

    no message

---
+++ JobsSwiftBaseConfigDemo/JobsSwiftBaseConfigDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobsSwiftBaseConfigDemoApp.swift
+//  JobsSwiftBaseConfigDemo
+//
+//  Created by Jobs on 2025/6/4.
+//
+
+import SwiftUI
+
+@main
+struct JobsSwiftBaseConfigDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JobsSwiftBaseConfigDemoTests/JobsSwiftBaseConfigDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  JobsSwiftBaseConfigDemoTests.swift
+//  JobsSwiftBaseConfigDemoTests
+//
+//  Created by Jobs on 2025/6/4.
+//
+
+import Testing
+@testable import JobsSwiftBaseConfigDemo
+
+struct JobsSwiftBaseConfigDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
