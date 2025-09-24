4ce1195458314a9e96ba80fbcedbc57a0c36b33c
Jobs <lg295060456@gmail.com>
2025-09-24 17:37:21 +0700

    no message

---
+++ JobsSwiftBaseConfigDemo/SwiftDefines/JobsCorConst.swift
@@ -0,0 +1,58 @@
+//
+//  JobsConst.swift
+//  JobsSwiftBaseConfigDemo
+//
+//  Created by Mac on 9/24/25.
+//
+
+class JobsCorConst: NSObject {
+    static let main_color: UIColor      = UIColor(named: "MainColor")!
+    // MARK: - 文本 626C90、A6B0C8
+    static let text0_color: UIColor      = UIColor(named: "TextColor0")!
+    static let text1_color: UIColor      = UIColor(named: "TextColor1")!
+    static let text2_color: UIColor      = UIColor(named: "TextColor2")!
+    static let text3_color: UIColor      = UIColor(named: "TextColor3")!
+    // MARK: -通用文本色
+    static let public_text_4: UIColor      = UIColor(named: "public-text-4")!
+    static let public_text_5: UIColor      = UIColor(named: "public-text-5")!
+    static let public_text_6: UIColor      = UIColor(named: "public-text-6")!
+    static let public_text_7: UIColor      = UIColor(named: "public-text-7")!
+    // MARK: -通用提示色
+    static let public_FZ_1: UIColor      = UIColor(named: "public-FZ-1")!
+    static let public_FZ_2: UIColor      = UIColor(named: "public-FZ-2")!
+    static let public_FZ_3: UIColor      = UIColor(named: "public-FZ-3")!
+    // MARK: -通用蒙版色
+    static let public_MASK: UIColor      = UIColor(named: "public-MASK")!
+    // MARK: -分割线
+    /// light: E2E2E2 dark:373A40
+    static let line0: UIColor      = UIColor(named: "Line0")!
+    // MARK: -背景
+    static let view_bg_color1: UIColor      = UIColor(named: "ViewColor1")!
+    static let view_bg_color2: UIColor      = UIColor(named: "ViewColor2")!
+    static let view_bg_color3: UIColor      = UIColor(named: "ViewColor3")!
+    static let view_bg_color4: UIColor      = UIColor(named: "ViewColor4")!
+    static let view_bg_color5: UIColor      = UIColor(named: "ViewColor5")!
+    static let view_bg_color6: UIColor      = UIColor(named: "ViewColor6")!
+    static let view_bg_color9: UIColor      = UIColor(named: "ViewColor9")!
+    static let view_bg_color10: UIColor      = UIColor(named: "ViewColor10")!
+    static let system_1:       UIColor      = UIColor(named: "system-1")!
+    static let system_2:       UIColor      = UIColor(named: "system-2")!
+    // MARK: -通用背景色
+    static let public_bg_7: UIColor      = UIColor(named: "public-BG-7")!
+    static let public_bg_8: UIColor      = UIColor(named: "public-BG-8")!
+    // MARK: 登陆、注册边框固定色
+    static let lineColor:UIColor = UIColor(hexString: "#ffffff",alpha: 0.1)!
+    static let img_unload: UIColor      =  UIColor(named: "img_unload") ?? UIColor(hexString: "D9E8FF")!
+    // MARK: vip背景色渐变
+    static let gradient_color_start: UIColor      = UIColor(hexString: "#353a3e")!
+    static let gradient_color_end: UIColor      = UIColor(hexString: "#2c2d2e")!
+
+    static let gradient_start: UIColor      = (UIColor(hexString: "#B0674A")?.withAlphaComponent(0.2))!
+    static let gradient_end: UIColor      = (UIColor(hexString: "#5639B4")?.withAlphaComponent(0.1))!
+
+    static let gradient_activity_start: UIColor      = (UIColor(hexString: "#ff284b")?.withAlphaComponent(0.7))!
+    static let gradient_activity_end: UIColor      = (UIColor(hexString: "#ff284b")?.withAlphaComponent(0.4))!
+    // MARK: 我的页面 vip渐变
+    static let gradient_vip_start: UIColor      = (UIColor(hexString: "#D2B592"))!
+    static let gradient_vip_end: UIColor      = (UIColor(hexString: "#F2DFCE"))!
+}
